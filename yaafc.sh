6bfa84d0927d47f74b54ba39d0012db9e74fdbbb
HocusYeetus <patrick.coral@epfl.ch>
2021-01-26 19:11:52 +0100

    Comments

--- yaafc.sh
+++ yaafc.sh
@@ -3,7 +3,8 @@
 FILE_PWM=$(echo /sys/class/drm/card0/device/hwmon/hwmon?/pwm1)
 FILE_MODE=$(echo /sys/class/drm/card0/device/hwmon/hwmon?/pwm1_enable)
 FILE_TEMP=$(echo /sys/class/drm/card0/device/hwmon/hwmon?/temp2_input)
-TEST_FILE=$(echo test.txt)
+
+$REFRESH=3
 
 function set_mode {
     echo "setting fan mode to $1"
@@ -17,15 +18,18 @@ fi
 
 set_mode 1
 
+# make the equation
 # Ax + B
+# values are multiplied by 100 to keep 2 decimals
 A=$((100 * 255 / ($2 - $1)))
 B=$((100 * $1 * 255 / ($2 - $1)))
 
-# echo $A
-# echo $B
+# result is divided by 100 to compensate for that
+# (A * 85 - B)/100
 
-echo $(((A * 85 - B)/100))
 
+# clamps to 10 or 255 PWM
+# prints temp and pwm for debug ;_;
 function calc_pwm {
     TEMP=$(cat $FILE_TEMP)
     TEMP=$((TEMP / 1000))
@@ -33,14 +37,15 @@ function calc_pwm {
     if [ $PWM -lt 10 ]
     then
         PWM=10
-    elif [ $PWM -gt 250 ]
+    elif [ $PWM -gt 255 ]
     then
         PWM=255
     fi
-    echo "$TEMP C : $PWM"
+    echo "$TEMP C : $PWM" 
     set_pwm $PWM
 }
 
+# change pwm value if different from previous value
 function set_pwm {
     OLD_PWM=$(cat $FILE_PWM)
     echo "old: $OLD_PWM"
@@ -50,6 +55,7 @@ function set_pwm {
     fi
 }
 
+# change back to auto upon exit
 function reset_on_exit {
     echo ""
     echo "Setting mode to auto"
@@ -60,7 +66,7 @@ function reset_on_exit {
 trap "reset_on_exit" SIGINT SIGTERM
 
 while true; do
-    sleep 1
+    sleep $REFRESH
     clear
     calc_pwm
 done
